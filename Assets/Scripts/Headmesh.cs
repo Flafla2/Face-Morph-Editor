d481c0bc164970b08b4691761b829bf358364362
Adrian <adrianbiagioli@gmail.com>
2015-11-11 17:57:39 -0500

    Initial

---
+++ Assets/Scripts/Headmesh.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Headmesh : MonoBehaviour {
+
+    public Morph[] Morphs
+    {
+        get { return _Morphs; }
+    }
+    private Morph[] _Morphs = new Morph[] {
+
+        new Morph { Name = "Size",          NameInternal = "neck_size",         HasNegativeValues = true,  Category = "Neck" },
+        new Morph { Name = "Size",          NameInternal = "upper_lip_size",    HasNegativeValues = false, Category = "Lip>Upper" },
+        new Morph { Name = "Size",          NameInternal = "lower_lip_size",    HasNegativeValues = false, Category = "Lip>Lower" },
+        new Morph { Name = "Depth",         NameInternal = "mouth_depth",       HasNegativeValues = true,  Category = "Mouth" },
+        new Morph { Name = "Width",         NameInternal = "mouth_width",       HasNegativeValues = true,  Category = "Mouth" },
+        new Morph { Name = "Height",        NameInternal = "nose_height",       HasNegativeValues = true,  Category = "Nose" },
+        new Morph { Name = "Angle",         NameInternal = "nose_angle",        HasNegativeValues = true,  Category = "Nose" },
+        new Morph { Name = "Depth",         NameInternal = "nose_depth",        HasNegativeValues = true,  Category = "Nose" },
+        new Morph { Name = "Depth",         NameInternal = "nose_tip_depth",    HasNegativeValues = true,  Category = "Nose>Tip" },
+        new Morph { Name = "Height",        NameInternal = "nose_tip_height",   HasNegativeValues = true,  Category = "Nose>Tip" },
+        new Morph { Name = "Width",         NameInternal = "chin_width",        HasNegativeValues = false, Category = "Chin" },
+        new Morph { Name = "Height",        NameInternal = "chin_height",       HasNegativeValues = false, Category = "Chin" },
+        new Morph { Name = "Hairline Puff", NameInternal = "hairline_puff",     HasNegativeValues = false, Category = "" },
+        new Morph { Name = "Fat",           NameInternal = "cheek_fat",         HasNegativeValues = true,  Category = "Cheek" },
+        new Morph { Name = "Depth",         NameInternal = "cheek_depth",       HasNegativeValues = true,  Category = "Cheek" }
+
+    };
+
+    [SerializeField]
+    private float[] MorphValues;
+    private SkinnedMeshRenderer SkinnedRenderer;
+
+    void Awake()
+    {
+        MorphValues = new float[Morphs.Length];
+        SkinnedRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+    
+    public void SetMorphValue(int morph_index, float value)
+    {
+        Morph morph = Morphs[morph_index];
+        if (morph.HasNegativeValues && value < 0)
+            return;
+        value = Mathf.Clamp01(value);
+
+        string shapename = morph.NameInternal;
+        if(morph.HasNegativeValues)
+        {
+            if (value > 0)
+                shapename += "_pos";
+            else
+                shapename += "_neg";
+        }
+
+        Mesh m = SkinnedRenderer.sharedMesh;
+        for(int x=0;x<m.blendShapeCount;x++)
+        {
+            if (m.GetBlendShapeName(x).Equals(shapename))
+            {
+                SkinnedRenderer.SetBlendShapeWeight(x, Mathf.Abs(value));
+                break;
+            }
+        }
+        
+        MorphValues[morph_index] = value;
+    }
+
+    public float GetMorphValue(int morph_index)
+    {
+        return MorphValues[morph_index];
+    }
+
+    public struct Morph
+    {
+        public string Name;
+        public string NameInternal;
+        public string Category;
+        public bool HasNegativeValues;
+        public float Value;
+    }
+}
---
+++ Assets/Scripts/HeadmeshEditor.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Headmesh))]
+public class HeadmeshEditor : Editor {
+
+    private MorphDirectory Root;
+    private Headmesh head;
+
+    public override void OnInspectorGUI()
+    {
+        head = target as Headmesh;
+
+        if(Root == null)
+        {
+            Root = new MorphDirectory();
+            for(int x=0;x<head.Morphs.Length;x++)
+                PlaceMorph(Root, head.Morphs[x], x);
+        }
+
+        TraverseTree(Root);
+    }
+
+    private void TraverseTree(MorphDirectory p)
+    {
+        if(!p.Path.Equals(""))
+        {
+            p.Open = EditorGUILayout.Foldout(p.Open, p.Name);
+
+            if (!p.Open)
+                return;
+        }
+        
+        foreach (MorphDirectory q in p.subdirectories)
+            TraverseTree(q);
+
+        for(int x=0;x<p.submorphs.Count;x++)
+        {
+            EditorGUI.BeginChangeCheck();
+
+            float slider = EditorGUILayout.Slider(
+                p.submorphs[x].Name,
+                head.GetMorphValue(p.submorph_indexes[x]),
+                p.submorphs[x].HasNegativeValues ? -1 : 0,
+                1);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(head, "Change Head Morph");
+                head.SetMorphValue(p.submorph_indexes[x], slider);
+                EditorUtility.SetDirty(head);
+            }
+        }
+    }
+
+    private bool PlaceMorph(MorphDirectory p, Headmesh.Morph q, int index)
+    {
+        if(p.Path.Equals(q.Category))
+        {
+            p.submorphs.Add(q);
+            p.submorph_indexes.Add(index);
+            return true;
+        }
+
+        foreach (MorphDirectory sub in p.subdirectories)
+        {
+            if (PlaceMorph(sub, q, index))
+            {
+                p.submorphs.Add(q);
+                p.submorph_indexes.Add(index);
+                return true;
+            }
+        }
+                
+
+        if(q.Category.StartsWith(p.Path))
+        {
+            MorphDirectory d = new MorphDirectory();
+            d.Name = q.Category.Substring(p.Path.Length).Split('>')[0];
+            d.Path = p.Path+'>'+d.Name;
+            p.subdirectories.Add(d);
+            return PlaceMorph(d,q, index);
+        }
+
+        return false;
+    }
+
+    private class MorphDirectory
+    {
+        public List<MorphDirectory> subdirectories;
+        public List<Headmesh.Morph> submorphs;
+        public List<int> submorph_indexes; // indexes in Headmesh.Morphs
+
+        public string Name = "";
+        public string Path = ""; // Uses > as path delimiter
+
+        public bool Open = false; // Open in the inspector
+
+        public MorphDirectory()
+        {
+            subdirectories = new List<MorphDirectory>();
+            submorphs = new List<Headmesh.Morph>();
+            submorph_indexes = new List<int>();
+        }
+    }
+}
